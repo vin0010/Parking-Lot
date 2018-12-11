43fc4d3f8f357c5629832db93371c87f5640b9ed
Vinoth G <vin10catchme@gmail.com>
2018-12-11 21:28:57 +0530

    added floor class

--- src/com/codex/pl/parking/Floor.java
+++ src/com/codex/pl/parking/Floor.java
@@ -1,5 +1,15 @@
 package com.codex.pl.parking;
 
+import java.util.List;
+
 public class Floor {
-	
+	private List<Integer> availableSlots;
+
+	public Floor(List<Integer> availableSlots) {
+		this.availableSlots = availableSlots;
+	}
+
+	public List<Integer> getAvailableSlots() {
+		return availableSlots;
+	}
 }
